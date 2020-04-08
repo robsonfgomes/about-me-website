3367a6049f758f9ba2581221a16bd1d2f05c7ef6
Robson Gomes <fgomes.robson@gmail.com>
2020-04-08 01:15:48 -0300

    Force redirect from www to http v2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 import Mobile from './components/Mobile';
 //import { Col, Row, Container } from 'react-bootstrap'
-import './redirect';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/redirect.js
+++
@@ -1,3 +0,0 @@
-if (window.location.href.indexOf("www") > -1) {
-    window.location.href = "http://robsonfgomes.com/";
-}
