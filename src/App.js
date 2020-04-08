a56c6857ee2433a071e7aa9a3774ee823787b2b1
Robson Gomes <fgomes.robson@gmail.com>
2020-04-08 01:07:46 -0300

    Added rule to force redirect from www to http

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar';
 import Content from './components/Content';
 import Mobile from './components/Mobile';
 //import { Col, Row, Container } from 'react-bootstrap'
+import './redirect';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
---
+++ src/redirect.js
@@ -0,0 +1,3 @@
+if (window.location.href.indexOf("www") > -1) {
+    window.location.href = "http://robsonfgomes.com/";
+}
